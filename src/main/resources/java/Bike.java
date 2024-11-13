66399b3a571d591af0713dfebf9b9dd2f9f836b2
Michal7779 <michal244@onet.eu>
2024-11-13 20:17:08 +0100

    123

---
+++ src/main/resources/java/Bike.java
@@ -0,0 +1,10 @@
+package pl.example.vehicles;
+
+public class Bike extends pl.example.pl.example.vehicles.Vehicle {
+
+public Bike(String name) {super(name) ;}
+
+
+@Override
+public int getMaxSpeed() {return 200; }
+}
---
+++ src/main/resources/java/Car.java
@@ -0,0 +1,18 @@
+package pl.example.vehicles;
+
+public class Car extends pl.example.pl.example.vehicles.Vehicle {
+
+private int doors;
+
+public Car( String name, int doors) {
+
+    super(name);
+    this.doors = doors;
+}
+
+@Override
+public int getMaxSpeed() {return 200; }
+public int getDoors() { return doors; }
+
+
+}
